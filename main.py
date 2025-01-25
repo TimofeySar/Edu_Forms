caa232f920d548664a13adaef2009c5a459a5487
dmitr <awd@adw>
2025-01-25 21:41:29 +0300

    check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #shek
-
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
